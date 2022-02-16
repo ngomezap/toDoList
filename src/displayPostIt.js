6d2b3b723cac26141d45ea1194797ce4d66cd6dc
ngomezap <igomez.ap@gmail.com>
2022-02-16 19:11:12 +0100

    Add list of tasks to posIt view

--- src/displayPostIt.js
+++ src/displayPostIt.js
@@ -1,14 +1,21 @@
 import { whiteboard } from "./createPostIt";
 import clear from './clear';
 
-const displayPostIt = function(){
+const displayPostIt = function(e){
     clear();
-    const postItName = this.firstChild.nodeValue;
-    whiteboard.listOfPostIts.forEach((e) => {
-        if (e.name === postItName){
-            const post = document.createElement('div');
-            post.innerText = postItName;
-            document.body.appendChild(post);
+    
+    whiteboard.listOfPostIts.forEach(postIt => {
+        if(postIt.name === this.firstChild.nodeValue){
+            const title = document.createElement('div');
+            title.innerText = postIt.name;
+            document.body.appendChild(title);
+
+            postIt.listOfTasks.forEach( task => {
+                const taskDiv = document.createElement('div');
+                taskDiv.innerText = task.text;
+                taskDiv.classList.add('task');
+                document.body.appendChild(taskDiv);
+            })
         }
     })
 }
