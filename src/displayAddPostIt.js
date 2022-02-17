480355f8637810ab8d4b2768da3929366000d3a8
ngomezap <igomez.ap@gmail.com>
2022-02-17 18:39:43 +0100

    Add funcionalities for ADD PostIt view

--- src/displayAddPostIt.js
+++ src/displayAddPostIt.js
@@ -2,33 +2,75 @@
 
 export default function displayAddPostIt(){
 
-    //Numer of tasks counter
-    const counter = 0;
+    //Number of tasks counter
+    let counter = 0;
 
-    //Create ADD form container
-    const addContainer = document.createElement('div');
-    addContainer.setAttribute('id', 'addContainer');
+    //Form container
+    const formContainer = document.createElement('div');
+    formContainer.setAttribute('id', 'formContainer');
 
+    //Title container
+    const titleContainer = document.createElement('div');
+    titleContainer.setAttribute('id', 'titleContainer');
 
+    //Tasks container
+    const tasksContainer = document.createElement('div');
+    tasksContainer.setAttribute('id', 'tasksContainer');
+
+    //Title
     const addTitle = document.createElement('input');
     const addTitleLabel = document.createElement('label');
     addTitle.setAttribute('type', 'text');
     addTitle.setAttribute('id', 'addTitle');
     addTitleLabel.setAttribute('for', 'addTitle');
     addTitleLabel.innerText = "Title";
+    titleContainer.appendChild(addTitleLabel);
+    titleContainer.appendChild(addTitle);
+
+    //Button Add Task
+    const btnAddTask = document.createElement('button');
+    btnAddTask.innerText = "Add Task";
+    btnAddTask.addEventListener('click', () =>{
+        const newTask = createTask(counter);
+        tasksContainer.appendChild(newTask.taskCont);
+        counter ++;
+    });
+
+    //Append all
+    formContainer.appendChild(titleContainer);
+    formContainer.appendChild(tasksContainer);
+    formContainer.appendChild(btnAddTask);
+    document.body.appendChild(formContainer);
+}
 
 
+function createTask(counter){
+    //Task container
+    const taskCont = document.createElement('div');
+    taskCont.setAttribute('id', `taskCont${counter}`);
+
+    //Task inpunt and label
     const addTask = document.createElement('input');
     const addTaskLabel = document.createElement('label');
     addTask.setAttribute('type', 'text');
     addTask.classList.add('addTask');
     addTask.setAttribute('id', `task${counter}`);
     addTaskLabel.setAttribute('for', `task${counter}`);
-    addTaskLabel.innerText = `Task ${counter}`;
+    addTaskLabel.innerText = "Task";
 
-    addContainer.appendChild(addTitleLabel);
-    addContainer.appendChild(addTitle);
-    addContainer.appendChild(addTaskLabel);
-    addContainer.appendChild(addTask);
-    document.body.appendChild(addContainer);
+    //Remove task button
+    const btnRmvTask = document.createElement('button');
+    btnRmvTask.innerText = "Delete";
+    btnRmvTask.addEventListener('click', removeTask);
+
+
+    taskCont.appendChild(addTaskLabel);
+    taskCont.appendChild(addTask);
+    taskCont.appendChild(btnRmvTask);
+
+    return {taskCont}
+}
+
+function removeTask(e){
+    e.target.parentNode.remove();  
 }
