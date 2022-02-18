07e399f7f6803af8edec5ec3958275f346d408d4
ngomezap <igomez.ap@gmail.com>
2022-02-18 18:05:03 +0100

    Create relation between the 3 Views

--- src/controller.js
+++ src/controller.js
@@ -1,18 +1,36 @@
 import { displayWhiteboard } from "./displayWhiteboard"
 import { displayPostIt } from "./displayPostIt"
 import displayAddPostIt from "./displayAddPostIt";
+import clear from './clear';
 
 const controller = function(){
     //Display the whiteboard by default
-    //displayWhiteboard();
+    displayWhiteboard();
 
-    displayAddPostIt();
-    /*
-    //Add event listeners to post its
+    //Create Listeners
+    listeners();
+
+}
+
+const listeners = function(){
+    const homeBtn = document.getElementById('homeBtn');
+    const addBtn = document.getElementById('addBtn');
     const wb = document.getElementById('whiteboard').querySelectorAll('.postIt');
+    
+    homeBtn.addEventListener('click', () => {
+        clear();
+        controller();
+    });
+    
+    addBtn.addEventListener('click',() => {
+        clear();
+        displayAddPostIt();
+    });
+
+    //Add event listeners to post its
     wb.forEach(e => {
         e.addEventListener('click', displayPostIt);
-    })*/
+    })
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import { controller } from "./controller";
-import clear from './clear';
 import './style.css'
 
 
 const nav = document.createElement('nav');
 const homeBtn = document.createElement('button');
+homeBtn.setAttribute('id', 'homeBtn');
 const addBtn = document.createElement('button');
+addBtn.setAttribute('id', 'addBtn');
 addBtn.innerText = "ADD";
 homeBtn.innerText = 'HOME';
 nav.appendChild(homeBtn);
 nav.appendChild(addBtn);
 document.body.appendChild(nav);
 
-homeBtn.addEventListener('click', () => {
-    clear();
-    controller();
-});
-
-addBtn.addEventListener('click', controller);
 
 controller();
